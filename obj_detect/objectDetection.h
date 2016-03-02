81fe203cc4b9808d3b4dca5dcc7f200f9b31ea4d
staslist <slistopa@uci.edu>
2016-03-01 21:41:27 -0800

    Further comment fixes.

--- obj_detect/objectDetection.h
+++ obj_detect/objectDetection.h
@@ -10,9 +10,9 @@
 #include "opencv2/imgproc.hpp"
 
 //Raspberry Pi Port Only
-//#include <raspicam/raspicam_cv.h>
-//#include "opencv2/photo.hpp"
-//#include "opencv2/core.hpp"
+#include <raspicam/raspicam_cv.h>
+#include "opencv2/photo.hpp"
+#include "opencv2/core.hpp"
 
 #include <iostream>
 #include <stdio.h>
